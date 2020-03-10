6104f64ebf93c710878c39e0f5f9622f13a728ac
Chloe Tasker <rc0038439@student.riversidecollege.ac.uk>
2020-03-10 12:39:38 +0000

    Add project files.

---
+++ Volumes/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Volumes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //Declare Variables
+            double Radius, Volume;
+
+            //Read values from text box
+            try
+            {
+                Radius = double.Parse(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Type a number in the Radius Box!");
+                Radius = 0.0;
+
+            }
+
+            //Calcualte Volume
+            Volume= 4.0 / 3.0 * Math.PI * Radius * Radius * Radius;
+
+            //Display Volume
+            Text = "Volume= = " + Volume + "m^3";
+
+            }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
